26b6e4dd166fa444e2681caedf2971168297efb3
parasop <63485824+parasop@users.noreply.github.com>
2024-07-15 21:55:11 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,48 @@
+<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-7 col-md-9">
+							<div id="colorlib-logo"><a href="index.html">Footwear</a></div>
+						</div>
+						<div class="col-sm-5 col-md-3">
+			            <form action="#" class="search-wrap">
+			               <div class="form-group">
+			                  <input type="search" class="form-control search" placeholder="Search">
+			                  <button class="btn btn-primary submit-search text-center" type="submit"><i class="icon-search"></i></button>
+			               </div>
+			            </form>
+			         </div>
+		         </div>
+					<div class="row">
+						<div class="col-sm-12 text-left menu-1">
+							<ul class="flex justify-between items-center">
+								<div class="flex space-x-4">
+									<li><a href="index.html">Home</a></li>
+									<li class="has-dropdown relative">
+										<a href="men.html">Men</a>
+										<ul class="dropdown absolute left-0 mt-2 space-y-2 bg-white shadow-lg p-4 hidden">
+											<li><a href="product-detail.html">Product Detail</a></li>
+											<li><a href="cart.html">Shopping Cart</a></li>
+											<li><a href="checkout.html">Checkout</a></li>
+											<li><a href="order-complete.html">Order Complete</a></li>
+											<li><a href="add-to-wishlist.html">Wishlist</a></li>
+										</ul>
+									</li>
+									<li><a href="women.html">Women</a></li>
+									<li class="active"><a href="about.html">About</a></li>
+									<li><a href="contact.html">Contact</a></li>
+								</div>
+								<div class="flex space-x-4">
+									<li><img class="w-10 h-10" src="./images/user.png" /></li>
+									<li><img class="w-10 h-10" src="./images/wishlist.png" /></li>
+							
+									<li class="cart"><a href="cart.html"><i class="icon-shopping-cart"></i> Cart [0]</a></li>
+								</div>
+							</ul>
+					</div>
+					</div>
+				</div>
+			</div>
+				</div>
+		</nav>
